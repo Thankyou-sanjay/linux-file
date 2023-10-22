bb5cceeef36c102adbd5c42b0ffed2819074f46e
sanjay <crazysanjay00@gmail.com>
2023-10-22 07:51:12 +0000

    done

---
+++ file-itrate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for FILE in *
+
+do 
+echo $FILE
+done
---
+++ function.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+add_user()
+{
+USER-1$
+PASS-2$
+
+useradd ~M ~P $USER $PASS && echo "Successfully added user"
+}
+
+#MAIN
+
+add_user sanjay san@8081
